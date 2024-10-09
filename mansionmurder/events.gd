93e42479d432a9b205c4bdf8e3f6cdcf076e50b1
Asilbek Norboev <asilbeknorboev@gmail.com>
2024-10-09 11:26:33 -0500

    inventoryUI added

--- mansionmurder/events.gd
+++ mansionmurder/events.gd
@@ -6,3 +6,14 @@ signal room_entered(room)
 func new_game():
 	print("game start")
 	#$Audio.play()
+
+
+func _ready():
+	var general_border_ui = $GeneralBorderUI
+	general_border_ui.inventorybuttonpressed.connect(_on_inventory_button_pressed)
+	$InventoryUI.visible = false
+
+
+func _on_inventory_button_pressed():
+	# Toggle visibility: if it's visible, hide it; if it's hidden, show it
+	$InventoryUI.visible = not $InventoryUI.visible
--- mansionmurder/general_border_ui.gd
+++ mansionmurder/general_border_ui.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 signal investigate
+signal inventorybuttonpressed
 
 func _on_investigate_button_pressed():
 	$InvestigateButton.modulate = Color("#000000")
@@ -7,3 +8,10 @@ func _on_investigate_button_pressed():
 	$InvestigateButton.modulate = Color("#ffffff")
 	investigate.emit()
 	
+
+
+func _on_inventory_button_pressed() -> void:
+	$InventoryButton.modulate = Color("#000000")
+	await get_tree().create_timer(0.2).timeout
+	$InventoryButton.modulate = Color("#ffffff")
+	inventorybuttonpressed.emit()
