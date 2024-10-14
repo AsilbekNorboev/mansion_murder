239c833c0b98fdc51106539a324630e3f8aaf068
ORK11D <100381769+ORK11D@users.noreply.github.com>
2024-10-14 16:24:41 -0500

    more dialogue fixes

--- mansionmurder/clue.gd
+++ mansionmurder/clue.gd
@@ -5,13 +5,15 @@ var textbox_scene = preload("res://textbox.tscn").instantiate()
 var textappear = textbox_scene.get_node("TextboxContainer")
 
 const lines:Array[String] = [
-	"A painting of a young woman."
+	"A painting of a young woman.",
+	"It looks old."
 ]
+
+var current_line_index = 0
 
 func _input(event):
 	if Input.is_action_pressed("click"):
 		if get_rect().has_point(to_local(event.position)):
 			print("You clicked on Clue")
 			add_child(textbox_scene)
-			#textappear.add_text(lines)
-			textappear.add_text("A painting of a young woman.")
+			textappear.add_text(lines[current_line_index])
