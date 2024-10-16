3514c0fb73d1a551eaf604745c10b94f0f4936de
ORK11D <100381769+ORK11D@users.noreply.github.com>
2024-10-16 15:04:23 -0500

    Revert "added rudimentary inventory_system"
    This reverts commit 50c20ef47a809d00e01bc35e30ae6d8c30ceeb43.

--- mansionmurder/events.gd
+++ mansionmurder/events.gd
@@ -3,22 +3,11 @@ extends Node
 signal room_entered(room)
 signal puzzle_completed(puzzle1)
 
-@onready var inventory_ui = $InventoryUI
-@onready var clue_node = $AddClue
-@onready var clue_node2 = $AddClue2
-
-func _on_clue_clicked(clue_data):
-	# Add the clue to the inventory UI
-	inventory_ui.add_clue(clue_data)
-	
-
 func new_game():
 	print("game start")
 
 
 func _ready():
-	clue_node.clue_clicked.connect(self._on_clue_clicked)
-	clue_node2.clue_clicked.connect(self._on_clue_clicked)
 	var general_border_ui = $GeneralBorderUI
 	general_border_ui.inventorybuttonpressed.connect(_on_inventory_button_pressed)
 	$InventoryUI.visible = false
