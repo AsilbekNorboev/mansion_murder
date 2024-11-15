641ce89da4cf4df1155f694cdb58e37e631f85e8
ORK11D <100381769+ORK11D@users.noreply.github.com>
2024-11-15 13:39:24 -0600

    tweaked settings menu

--- mansionmurder/settings_ui.gd
+++ mansionmurder/settings_ui.gd
@@ -3,8 +3,9 @@ extends Control
 var volume := 100
 
 func _ready():
-	$MarginContainer/VBoxContainer/Volume.value = volume
+	$"Audio Container/VolumeBoxContainer/Volume".value = volume
 	_update_audio_volumes()
+	
 func _on_volume_value_changed(value: float) -> void:
 	volume = value
 	_update_audio_volumes()
@@ -15,3 +16,13 @@ func _on_mute_toggled(toggled_on: bool) -> void:
 
 func _update_audio_volumes():
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), lerp(-40, 0, volume / 100.0))
+
+#waits a few seconds to allow the sound effect of the button to play
+func wait(seconds: float) -> void:
+	OS.delay_msec(seconds * 1000)
+	
+
+func _on_exit_game_pressed() -> void:
+	$ButtonClick.play()
+	wait(.5)
+	get_tree().quit()
