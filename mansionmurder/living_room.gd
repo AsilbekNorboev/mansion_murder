7f25545e1081be70ff04b0d30b38946f0b852cd4
ORK11D <100381769+ORK11D@users.noreply.github.com>
2024-12-01 21:12:56 -0600

    sfx

--- mansionmurder/living_room.gd
+++ mansionmurder/living_room.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	Events.room_entered.emit(self)
+
+
+func _on_piano_button_pressed() -> void:
+	$Piano.play() # Replace with function body.
