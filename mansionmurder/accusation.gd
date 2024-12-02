6b0b604a4500de27ae5c7912725d4185d6104813
Asilbek Norboev <asilbeknorboev@gmail.com>
2024-12-02 12:25:48 -0600

    fixed the text getting stuck at * for accusations

--- mansionmurder/accusation.gd
+++ mansionmurder/accusation.gd
@@ -32,19 +32,23 @@ func wait(seconds: float) -> void:
 func _on_chef_button_pressed() -> void:
 	$ButtonClick.play()
 	wait(.5)
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Accusation Scenes/chef_accused.tscn")
 
 func _on_gardener_button_pressed() -> void:
 	$ButtonClick.play()
 	wait(.5)
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Accusation Scenes/gardener_accused.tscn")
 
 func _on_wife_button_pressed() -> void:
 	$ButtonClick.play()
 	wait(.5)
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Accusation Scenes/wife_accused.tscn")
 
 func _on_maid_button_pressed() -> void:
 	$ButtonClick.play()
 	wait(.5)
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Accusation Scenes/maid_accused.tscn")
