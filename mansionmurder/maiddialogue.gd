327a9d76f9ee043fb47c0d3a814fd168c962e10a
ORK11D <100381769+ORK11D@users.noreply.github.com>
2024-12-02 13:22:58 -0600

    added interactable background that calls text but isnt clue

--- mansionmurder/maiddialogue.gd
+++ mansionmurder/maiddialogue.gd
@@ -2,18 +2,21 @@ extends Node
 
 @export var dialog_dictionary = {
 	"": ["Oh, no... no, this can’t be happening. I can’t believe he’s gone.",
-		"He didn’t deserve this... He was always so kind, always looking out for everyone. This... this isn’t fair."],
+		"He didn’t deserve this... He was always so kind, always looking out for everyone. This...isn’t fair.",
+			
+		"You’re welcome to look through the bedroom. I’ve been cleaning here, maybe you’ll find something that can assist in your investigation."],
 	
 	"Knife": 
 		["That knife... it’s from the kitchen, isn’t it? But... he never went near it. He always had me or the chef do the work. Who would bring that here?", "But his wife... well, she always had a lot of sharp words to say, especially when she was upset."],
 		
 	"Dirt":
-		["Dirt in the kitchen? How... how could that happen? I always make sure to clean up, and I’ve never seen anything like that. Maybe the gardener... ",
-		"That doesn’t sound right. Could he have... dropped it somehow? I don’t know."],
+		["Dirt in the kitchen? How could that happen? I always make sure I tidy every part of the mansion. Maybe the gardener tracked some dirt inside, have you asked him?"],
 		
 	"Gloves":
-		["Those... those are mine? But I swear, I would never—never hurt him! He was always kind to me.",
-		"You need to look at her—the wife. She’s the one who’s always been so cold toward him, always angry. She’s been on edge lately.",
+		["Those are mine...where did you find them? Blood?! Oh no, no, I swear, I would never—never hurt him! He was always kind to me."],
+		#"You need to look at her—the wife. She’s the one who’s always been so cold toward him, always angry. She’s been on edge lately.",
 		
-		"You’re welcome to look through the bedroom. I’ve been cleaning here, maybe you’ll find something that makes more sense...if you look close enough"]
+	"Diary":
+		["Ah, that's the lady's diary! I...I couldn't possibly look inside, that would be highly inappropriate of me!"],
+	
 }
